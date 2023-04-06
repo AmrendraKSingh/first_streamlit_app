82ab1f65dc263bb5e50246bf40eab55a29a5898c
AmrendraKSingh <39608544+AmrendraKSingh@users.noreply.github.com>
2023-04-06 14:13:11 +0530

    Update streamlit_app.py
    Correction Function insert_row_snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ if streamlit.button('Get fruit load list'):
 def insert_row_snowflake(new_fruit) : 
    with my_cnx.cusor() as my_cur:
       my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ( 'from streamlit')")
-      return 'Thanks for adding '+ add_my_furit
+      return 'Thanks for adding '+ new_fruit
    
    
    
