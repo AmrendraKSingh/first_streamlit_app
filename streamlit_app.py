2acccc7620194836366616ab4e7522743a22e345
AmrendraKSingh <39608544+AmrendraKSingh@users.noreply.github.com>
2023-04-06 08:32:12 +0530

    Update streamlit_app.py
    Display all item in fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,6 @@ my_cur = my_cnx.cursor()
 # streamlit.text(my_data_row)
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list") ; 
 my_data_row = my_cur.fetchone();
+my_data_rows = my_cur.fetchall();
 streamlit.text("The fruit load list contains") ;
-streamlit.text(my_data_row);
+streamlit.text(my_data_rows);
